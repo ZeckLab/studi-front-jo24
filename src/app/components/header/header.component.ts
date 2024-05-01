6622b0abffd2f9ef9d484382291a1a312683fb2b
ZeckLab <sturbout@gmail.com>
2024-05-01 23:45:28 +0200

    Cleaning configuration and routing files + adding scss files

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrl: '/src/scss/components/header.scss',
+})
+export class HeaderComponent {
+
+}
