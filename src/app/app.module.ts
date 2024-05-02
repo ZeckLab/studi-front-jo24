083d25e796e68ead351967d3e7a09f58623100a1
ZeckLab <sturbout@gmail.com>
2024-05-03 00:55:57 +0200

    Adding footer HTML and SCSS. Version Desktop.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
@@ -11,6 +12,7 @@ import { HttpClientModule } from '@angular/common/http';
     declarations: [
         AppComponent,
         HeaderComponent,
+        FooterComponent,
         ],
     
     providers: [],
