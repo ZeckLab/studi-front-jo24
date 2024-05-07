0f8e82707a6009ee54be26089aeff73d175f095e
ZeckLab <sturbout@gmail.com>
2024-05-07 08:24:44 +0200

    feature/Footer - Adding hover on the social icons.

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -8,6 +8,10 @@ import { HttpClient } from '@angular/common/http';
 })
 export class FooterComponent implements OnInit {
 
+  rootImg: string = "assets/images/logo/";
+  hoverImg: string = "-gold";
+  extensionImg: string = ".svg";
+
   partnersData:any;
   url: string = "../../../assets/data/official-partners-list.json";
 
@@ -17,6 +21,14 @@ export class FooterComponent implements OnInit {
     this.http.get(this.url).subscribe(data => {
       this.partnersData = data;
     });
+  }
+
+  fctMouseOver(event: any, name:string) : void {
+    event.srcElement.src = event.srcElement.src.replace(name, name + "-gold");
+  }
+ 
+  fctMouseOut(event: any, name:string) : void {
+    event.srcElement.src = event.srcElement.src.replace(name, "");
   }
 
 }
