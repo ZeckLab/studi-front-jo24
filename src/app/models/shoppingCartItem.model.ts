f13df4b2aa518bb3e4473fd68480b6c3a8ebf2b4
ZeckLab <sturbout@gmail.com>
2024-05-25 15:52:07 +0200

    feature/cartValidation - added two buttons to clear the cart and validate the cart. Save the cart as soon as a modification in the local storage.

--- src/app/models/shoppingCartItem.model.ts
+++ src/app/models/shoppingCartItem.model.ts
@@ -4,4 +4,37 @@ import { Offer } from './offer.model';
 export class ShoppingCartItem {
     offer!: Offer;
     quantity: number = 0;
+
+    constructor(offer: Offer, quantity: number) {
+        this.offer = offer;
+        this.quantity = quantity;
+    }
+
+    addItemQtyStorage(quantity: number) {
+        let itemsArray = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')!) : [];
+        let itemCart = itemsArray.find((item: { offer: { title: string; }; }) => item.offer.title === this.offer.title);
+        if (itemCart) {
+            itemCart.quantity += quantity;
+        } else {
+            itemsArray.push({ offer: this.offer, quantity: quantity });
+        }
+
+        localStorage.setItem('cart', JSON.stringify(itemsArray));
+    }
+
+    removeItemQtyStorage(quantity: number) {
+        let itemsArray = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')!) : [];
+        let itemCart = itemsArray.find((item: { offer: { title: string; }; }) => item.offer.title === this.offer.title);
+        if (itemCart) {
+            let newQuantity = itemCart.quantity - quantity;
+            if(newQuantity > 0) {
+                itemCart.quantity = newQuantity;
+            } else {
+                itemsArray = itemsArray.filter((item: { offer: { title: string; }; }) => item.offer.title !== this.offer.title);
+            }
+        }
+
+        localStorage.setItem('cart', JSON.stringify(itemsArray));
+    }
+
 }
