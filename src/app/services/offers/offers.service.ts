059ff2fd0f70ab7ab05d3458d5c009f6f66414ad
ZeckLab <sturbout@gmail.com>
2024-05-31 10:58:47 +0200

    Adding an offer-input component to display the components in admin mode - edit mode. Adding functions getOfferByTitle, update, add and patch visibility. Style on the offer-input component.

--- src/app/services/offers/offers.service.ts
+++ src/app/services/offers/offers.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Offer } from '../../models/offer.model';
+import { AuthenticateService } from '../authenticate/authenticate.service';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,7 @@ export class OffersService {
 
   endpointURL = environment.api + 'offers';
 
-  constructor(private httpClient: HttpClient) {
+  constructor(private httpClient: HttpClient, private authService: AuthenticateService) {
   }
 
   getAll() {
@@ -20,7 +21,7 @@ export class OffersService {
       this.httpClient.get(this.endpointURL).subscribe({
         next: (data: any) => {
           const offers = [];
-          for(const jsonOffer of data){
+          for (const jsonOffer of data) {
             const offer = new Offer();
             offer.loadfromJson(jsonOffer);
             offers.push(offer);
@@ -29,8 +30,8 @@ export class OffersService {
           observer.complete();
         },
         error: error => {
-        observer.error(error);
-        observer.complete();
+          observer.error(error);
+          observer.complete();
         }
       });
     });
@@ -43,25 +44,25 @@ export class OffersService {
         next: (data: any) => {
           const offers = [];
 
-          for(const jsonOffer of data){
+          for (const jsonOffer of data) {
             const offer = new Offer();
             offer.loadfromJson(jsonOffer);
             offers.push(offer);
           }
-          
+
           observer.next(offers);
           observer.complete();
         },
         error: error => {
-        console.log(error);
-        observer.error(error);
-        observer.complete();
+          console.log(error);
+          observer.error(error);
+          observer.complete();
         }
       });
     });
   }
 
-  get(id: number){
+  get(id: number) {
     return new Observable<Offer>(observer => {
       this.httpClient.get(this.endpointURL + '/' + id).subscribe({
         next: (data: any) => {
@@ -77,4 +78,92 @@ export class OffersService {
       });
     });
   }
+
+  // Check if the offer title already exists
+  getByTitle(title: string) {
+    return new Observable<boolean>(observer => {
+      this.httpClient.get(this.endpointURL + '/title/' + title).subscribe({
+        next: (data: any) => {
+          observer.next(data.exist);
+          observer.complete();
+        },
+        error: (error) => {
+          observer.error(error);
+          observer.complete();
+        }
+      });
+    });
+  }
+
+  // patch the visibility of an offer
+  toggleVisible(id: number, visible: boolean) {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + this.authService.getToken
+    });
+
+    return new Observable<Offer>(observer => {
+      this.httpClient.patch(this.endpointURL + '/' + id + '/visible', { visible: visible }, { headers }).subscribe({
+        next: (data: any) => {
+          const offer = new Offer();
+          offer.loadfromJson(data);
+          observer.next(offer);
+          observer.complete();
+        },
+        error: (error) => {
+          console.error(error);
+          observer.error(error);
+          observer.complete();
+        }
+      });
+    });
+  }
+
+  // update an offer
+  update(offer: Offer) {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + this.authService.getToken
+    });
+
+    return new Observable<Offer>(observer => {
+      this.httpClient.put(this.endpointURL + '/' + offer.offer_id, offer, { headers }).subscribe({
+        next: (data: any) => {
+          const offer = new Offer();
+          offer.loadfromJson(data);
+          observer.next(offer);
+          observer.complete();
+        },
+        error: (error) => {
+          observer.error(error);
+          observer.complete();
+        }
+      });
+    });
+  }
+
+  // add an offer
+  add(offer: Offer) {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + this.authService.getToken
+    });
+
+    return new Observable<Offer>(observer => {
+      this.httpClient.post(this.endpointURL, offer, { headers }).subscribe({
+        next: (data: any) => {
+          const offer = new Offer();
+          offer.loadfromJson(data);
+          observer.next(offer);
+          observer.complete();
+        },
+        error: (error) => {
+          observer.error(error);
+          observer.complete();
+        }
+      });
+    });
+  }
+
+
 }
